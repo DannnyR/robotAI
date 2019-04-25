7b3122862e2baad612a4e46305b5b9132ad6226b
DAN <indydsr@gmail.com>
2019-04-25 02:29:26 -0400

    Add files via upload

--- robotAI_game.py
+++ robotAI_game.py
@@ -2,25 +2,82 @@
 
 import random
 import arcade
+from pymunk import Vec2d
 
 # --- Constants ---
-SPRITE_SCALING_HERO = 0.5
-SPRITE_SCALING_ROBOT = 0.9
-ROBOT_COUNT = 60
+SPRITE_SCALING_HERO = 0.3
+SPRITE_SCALING_ROBOT = 0.6
+ROBOT_COUNT = 20
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 
+MOVE_MAP = {
+        arcade.key.UP: Vec2d(0,1),
+        arcade.key.DOWN: Vec2d(0,1),
+        arcade.key.LEFT: Vec2d(-1, 0),
+        arcade.key.RIGHT: Vec2d(1, 0),
+        }
+
 class Robot(arcade.Sprite):
     # Robots sprite creation
-    def update(self):
-        self.center_y -= 1
+    def __init__(self, filename, sprite_scaling):
+        # self.center_y -= 1 CODE MOVED TO NEW FUNCTION
         # Off screen - BRINGS ROBOTS BACK AROUND FOR ANOTHER PASS
-        if self.top < 0:
+        # if self.top < 0: MOVED TO UPDATE DEF BELOW IN SAME CLASS
+        super().__init__(filename, sprite_scaling)
             # THIS RESETS ROBOT SO THEY NEVER STOP
-            self.center_y = random.randrange(SCREEN_HEIGHT + 20,
-                                             SCREEN_HEIGHT + 100)
-            self.center_x = random.randrange(SCREEN_WIDTH)
+        self.center_y = 0
+        self.center_x = 0
+            
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        # Off screen - Bounce ROBOTS BACK AROUND FOR ANOTHER PASS
+        if self.left < 0:
+            self.change_x *= -1
+            
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
+    """
+    def Smiles(self):
+        # THE SMILE FACE
+        x = 300; y= 300; radus = 200
+        arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
+        # DRAW R-EYE
+        x = 370; y = 350; radius = 20
+        arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+        # DRAW L-EYE
+        x = 230; y = 350; radius = 20
+        arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
+        # DRAW THE BULLET HOLE
+        x = 115; y = 175; radius = 30
+        arcade.draw_circle_filled(x, y, radius, arcade.color.RED)
+        # SMILE - HERO
+        x = 300; y = 280; width = 120; height = 100
+        start_angle = 190; end_angle = 350; line_width = 10
+        arcade.draw_arc_outline(x, y, width, height, arcade.color.black,
+                                start_angle, end_angle, line_width)
+    def switch(self, x, y):
+        """
+         # This function creates the switches that shut down the AI
+        """
+        # Draw lever on the wall
+        arcade.draw_triangle_filled(x + 40, y,
+                                    x, y - 100,
+                                    arcade.color.Dark_RED)
+        # Draw switch column
+        arcade.draw_rectangle_filled(x + 30, x + 50,
+                                          y - 100, y - 140,
+                                          arcade.color.WHITE)
+        
+       """     
 
 class View(arcade.Window):
     # Window creation class
@@ -55,18 +112,24 @@ The machine spirits have awoken; to the detriment of mankind!")
 
         # setup HERO - hat tip Collin
         self.hero_sprite = arcade.Sprite("Gun_Knight.gif", SPRITE_SCALING_HERO)
-        self.hero_sprite.center_x = 80
+        # The below positions the Hero
+        self.hero_sprite.center_x = 50
         self.hero_sprite.center_y = 50
+        # Adds Hero to list
         self.hero_list.append(self.hero_sprite)
+        # self.draw_switch.random.randrange(SCREEN_WIDTH)
+        # self.draw_Smiles.left_y = 0
 
-        # creat robot - hat tip Dan
+        # create robot - hat tip Dan
         for i in range(ROBOT_COUNT):
 
             # Robots created
             robot = Robot("ROBOT-MARK1.gif", SPRITE_SCALING_ROBOT)
             # ROBOT PLACED ON MAP
             robot.center_x = random.randrange(SCREEN_WIDTH)
-            robot.center_y = random.randrange(SCREEN_HEIGHT)
+            robot.center_y = random.randrange(SCREEN_WIDTH)
+            robot.change_x = random.randrange(-1, 2)
+            robot.change_y = random.randrange(-2, 1)
             # ADD ROBOTS TO LIST
             self.robot_list.append(robot)
      
@@ -81,13 +144,21 @@ The machine spirits have awoken; to the detriment of mankind!")
         # Text Creation on Screen
         output = f"Score: {self.score}"
         arcade.draw_text(output, 10, 20, arcade.color.BLACK, 14)
+        """
+        self.wall_list.draw()
+        This will be the code for the walls
+        """
 
     def on_mouse_motion(self, x, y, dx, dy):
         """MOUSE CTRL COMMANDS"""
         # Hero Mouse Matched
         self.hero_sprite.center_x = x
-        self.hero_sprite.center_y = y # Had incorrect x on this limited side to side movement
-
+        self.hero_sprite.center_y = y
+    
+        # Mouse buttons enabled
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        pass
+    
     def update(self, delta_time):
         """Moving Logic for Game"""
         # ****FOR NOW: Sprites called limited movement!!!!!!!
@@ -103,6 +174,19 @@ The machine spirits have awoken; to the detriment of mankind!")
             robot.center_y = random.randrange(SCREEN_HEIGHT + 20,
                                               SCREEN_HEIGHT + 100)
             robot.center_y = random.randrange(SCREEN_WIDTH)
+class KeysPressed(self):
+       # Keyboard press button
+    def __init__(self, key, key_modifiers):
+        # Pymunk movement utils
+        self.keys = {k: False for k in MOVE_MAP}
+    def apply_movement(speed, dt, current_position: Vec2d, kp: KeysPressed) -> Vec2d:
+        return current position + delta_position * speed * dt
+    def apply_movement_norm(speed, dt, current_position: Vec2d, kp: KeysPressed) -> Vec2d:
+        return current_position + delta_position.normalized() * speed * dt
+    
+        
+            
+        pass # PLACEHOLDER
 
 def main():
     # Main Event
@@ -114,6 +198,8 @@ def main():
     # 4. KEEPS WINDOW ALIVE UNTIL KILLED
     # arcade.run()
     # Robot(Window)
+    # draw_Smiles(25, 250)
+    # draw_switch(100, 70)
 
 if __name__ == "__main__":
     main()
