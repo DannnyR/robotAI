7f1df3d1b058c8c836210f8ca10c8199bddc38a6
DAN <indydsr@gmail.com>
2019-04-30 16:33:11 -0400

    Add files via upload

--- robotAI_game.py
+++ robotAI_game.py
@@ -9,8 +9,8 @@ SPRITE_SCALING_HERO = 0.3
 SPRITE_SCALING_ROBOT = 0.6
 ROBOT_COUNT = 20
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 1800
+SCREEN_HEIGHT = 700
 
 
 
@@ -42,40 +42,6 @@ class Robot(arcade.Sprite):
             self.change_y *= -1
 
 
-"""
-    def Smiles(self):
-        # THE SMILE FACE
-        x = 300; y= 300; radus = 200
-        arcade.draw_circle_filled(x, y, radius, arcade.color.YELLOW)
-        # DRAW R-EYE
-        x = 370; y = 350; radius = 20
-        arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
-        # DRAW L-EYE
-        x = 230; y = 350; radius = 20
-        arcade.draw_circle_filled(x, y, radius, arcade.color.BLACK)
-        # DRAW THE BULLET HOLE
-        x = 115; y = 175; radius = 30
-        arcade.draw_circle_filled(x, y, radius, arcade.color.RED)
-        # SMILE - HERO
-        x = 300; y = 280; width = 120; height = 100
-        start_angle = 190; end_angle = 350; line_width = 10
-        arcade.draw_arc_outline(x, y, width, height, arcade.color.black,
-                                start_angle, end_angle, line_width)
-    def switch(self, x, y):
-        """
-         # This function creates the switches that shut down the AI
-"""
-        # Draw lever on the wall
-        arcade.draw_triangle_filled(x + 40, y,
-                                    x, y - 100,
-                                    arcade.color.Dark_RED)
-        # Draw switch column
-        arcade.draw_rectangle_filled(x + 30, x + 50,
-                                          y - 100, y - 140,
-                                          arcade.color.WHITE)
-        
-       """     
-
 class View(arcade.Window):
     # Window creation class
 
@@ -98,7 +64,7 @@ The machine spirits have awoken; to the detriment of mankind!")
         self.set_mouse_visible(False)
 
         # Background color
-        arcade.set_background_color(arcade.color.AMAZON)
+        arcade.set_background_color(arcade.color.ASH_GREY)
 
     def setup(self):
         """Starts game initializing vars"""
@@ -153,8 +119,14 @@ The machine spirits have awoken; to the detriment of mankind!")
         self.hero_sprite.center_y = y
     
         # Mouse buttons enabled
-    def on_mouse_press(self, x, y, button, key_modifiers):
-        pass
+    def on_mouse_press(self, x, y, button, modifiers):
+        # Mouse buttons
+        if button == arcade.MOUSE_BUTTON_LEFT:
+            print("LEFT MOUSE BUTTON PRESSED AT", x, y)
+        elif button == arcade.BUTTON_BUTTON_RIGHT:
+            print("RIGHT MOUSE BUTTON PRESSED AT", x, y)
+        
+        
     
     def update(self, delta_time):
         """Moving Logic for Game"""
@@ -172,18 +144,20 @@ The machine spirits have awoken; to the detriment of mankind!")
                                               SCREEN_HEIGHT + 100)
             robot.center_y = random.randrange(SCREEN_WIDTH)
        # Keyboard press button / CONTROLS
+       
     def Key_Pressed(self, key, modifiers):
         # CALLED WHEN USER PRESSES KEYS
         if key == arcade.key.LEFT:
             self.hero.change_x = -MOVEMENT_SPEED
         elif key == arcade.key.RIGHT:
-            self.hero.change_y = MOVEMENT_SPEED
+            self.hero.change_x = MOVEMENT_SPEED
         elif key == arcade.key.UP:
             self.hero.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
             self.hero.change_y = -MOVEMENT_SPEED
 
     def Key_Release(self, key, modifiers):
+        # When key released
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
             self.hero.change_x = 0
         elif key == arcade.UP or key == arcade.key.DOWN:
